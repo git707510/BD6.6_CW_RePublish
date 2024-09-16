5c6b84983d9cd50829fcfcfd53a62e75141c0a32
git707510 <keshawkumar707510@gmail.com>
2024-09-16 13:48:44 +0530

    move all files outside

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-// run `node index.js` in the terminal
+const cors = require('cors');
+const express = require('express');
+const { getAllEmployees, getEmployeesById } = require('./controllers');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//Endpoint to get all employees
+
+app.get('/employees', async (req, res) => {
+  const employees = getAllEmployees();
+  res.json({ employees });
+});
+
+//Endpoint to get employee details by id
+app.get('/employees/details/:id', async (req, res) => {
+  let employee = getEmployeesById(parseInt(req.params.id));
+
+  res.json({ employee });
+});
+
+module.exports = { app };
